454f1355b643061c7c756e31f4af467f26d92577
Edward Schults <edward.schults@gmail.com>
2014-11-24 14:12:55 +0100

    Exo1

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,19 @@
+class CoachingController < ApplicationController
+  def answer
+    @query = params[:query]
+    @answer = coach_answer(@query)
+  end
+
+  def ask
+  end
+
+  def coach_answer(your_message)
+    if your_message == "I am going to work right now!" || your_message == "I am going to work right now!".upcase
+      return ""
+    elsif your_message.end_with?("?")
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+end
